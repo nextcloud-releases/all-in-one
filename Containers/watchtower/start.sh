b3e6907b4e555ec6882079e39454946ea864336a
GitHub Workflow <workflow@github.com>
2022-01-18 22:49:40 +0000

    Repo-sync

---
+++ Containers/watchtower/start.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# Check if socket is available and readable
+if ! [ -a "/var/run/docker.sock" ]; then
+    echo "Docker socket is not available. Cannot continue."
+    exit 1
+elif ! test -r /var/run/docker.sock; then
+    echo "Docker socket is not readable by the root user. Cannot continue."
+    exit 1
+fi
+
+if [ -n "$CONTAINER_TO_UPDATE" ]; then
+    exec /watchtower --cleanup --run-once "$CONTAINER_TO_UPDATE"
+else
+    echo "'CONTAINER_TO_UPDATE' is not set. Cannot update anything."
+    exit 1
+fi
+
+
+exec "$@"
