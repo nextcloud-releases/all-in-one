06e5362ef999fe69e5dc8a1dd71cb86ebd9b2b26
szaimen <szaimen@e.mail.de>
2022-04-19 19:56:26 +0200

    add it to the onboot script
    Signed-off-by: szaimen <szaimen@e.mail.de>

--- .build/digitalocean/scripts/01-nextcloud-aio.sh
+++ .build/digitalocean/scripts/01-nextcloud-aio.sh
@@ -6,19 +6,3 @@ curl -fsSL get.docker.com | sudo sh
 # Adjust permissions
 sudo mkdir -p /mnt/ncdata
 sudo chown -R 33:0 /mnt/ncdata
-
-# Some Info
-cat << EOF > /etc/motd
- #    #  ######  #    #   #####   ####   #        ####   #    #  #####
- ##   #  #        #  #      #    #    #  #       #    #  #    #  #    #
- # #  #  #####     ##       #    #       #       #    #  #    #  #    #
- #  # #  #         ##       #    #       #       #    #  #    #  #    #
- #   ##  #        #  #      #    #    #  #       #    #  #    #  #    #
- #    #  ######  #    #     #     ####   ######   ####    ####   #####
-
-If you point a domain to this server (\$(hostname -I | cut -f1 -d' ')), you can open the admin interface at https://yourdomain.com:8443
-Otherwise you can open the admin interface at https://\$(hostname -I | cut -f1 -d' '):8080
-    
-Further documentation is available here: https://github.com/nextcloud/all-in-one
-
-EOF
