785a9c7b0e44af9fe30f852aeba5afa2d3bba963
szaimen <szaimen@e.mail.de>
2022-07-01 17:16:19 +0200

    add google cloud
    Signed-off-by: szaimen <szaimen@e.mail.de>

---
+++ .build/google-cloud/build-vm.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+# Based on https://cloud.google.com/sdk/docs/install#linux
+curl -O https://dl.google.com/dl/cloudsdk/channels/rapid/downloads/google-cloud-cli-392.0.0-linux-x86_64.tar.gz
+tar -xf google-cloud-cli-392.0.0-linux-x86_64.tar.gz
+./google-cloud-sdk/install.sh --usage-reporting false --path-update true --command-completion false --quiet
+./google-cloud-sdk/bin/gcloud init
+# log in with the marketplaces account
+# choose to manage the nextcloud-aio project
+# or manually with gcloud config set project nextcloud-aio
+gcloud config set project nextcloud-aio
+
+# All below is based on https://cloud.google.com/build/docs/building/build-vm-images-with-packer#yaml
+
+#
+# APIs aktivieren
+#
+
+# gcloud services enable sourcerepo.googleapis.com
+# gcloud services enable compute.googleapis.com
+# gcloud services enable servicemanagement.googleapis.com
+# gcloud services enable storage-api.googleapis.com
+
+#
+# IAM permissions
+#
+
+CLOUD_BUILD_ACCOUNT=$(gcloud projects get-iam-policy nextcloud-aio --filter="(bindings.role:roles/cloudbuild.builds.builder)"  --flatten="bindings[].members" --format="value(bindings.members[])")
---
+++ .build/google-cloud/readme.md
@@ -0,0 +1,2 @@
+
+The cloud builder files are inspired by https://github.com/GoogleCloudPlatform/cloud-builders-community/tree/master/packer/examples/gce
