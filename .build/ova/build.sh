33af1c8974ac8e9d46a9203d2fef8d7eceea07da
szaimen <szaimen@e.mail.de>
2022-07-27 01:04:53 +0200

    fix it differently
    Signed-off-by: szaimen <szaimen@e.mail.de>

--- .build/ova/build.sh
+++ .build/ova/build.sh
@@ -58,5 +58,4 @@ rm -rf /home/ncadmin/.ssh/authorized_keys
 touch /etc/ssh/revoked_keys
 chmod 600 /etc/ssh/revoked_keys
 docker stop nextcloud-aio-mastercontainer
-set +e
-rm /var/lib/docker/volumes/nextcloud_aio_mastercontainer/_data/certs/ssl.*
+rm -f /var/lib/docker/volumes/nextcloud_aio_mastercontainer/_data/certs/ssl.*
